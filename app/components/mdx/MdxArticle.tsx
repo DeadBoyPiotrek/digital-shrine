5d773eda7b29fe8cbd4e439c826391565e99dcd0
Pio trek <pipolito2115@gmail.com>
2022-12-16 14:24:11 +0100

    feat: change Mdx article location

--- app/components/mdx/MdxArticle.tsx
+++ app/components/mdx/MdxArticle.tsx
@@ -2,12 +2,12 @@
 import { MDXProvider } from '@mdx-js/react';
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
 import Image from 'next/image';
-import { Link } from './Link';
+// import { Link } from './Link';
 interface MDXContentProps {
   source: MDXRemoteProps;
 }
 
-const shortcodes = { Image, a: Link };
+const shortcodes = { Image };
 
 export default function MDXContent({ source }: MDXContentProps) {
   return (
